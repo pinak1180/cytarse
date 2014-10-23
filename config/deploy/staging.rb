fc8d2a4b880a3cdcc3226cbb696f7bf65ae7fcd9
MattHall <matthew.hall@gmail.com>
2014-10-23 10:33:47 +0100

    Move to new host

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,4 +2,4 @@ set :branch, 'feature/deploy'
 set :rails_env, 'production'
 set :deploy_to, '/home/app/heliwm_staging'
 
-server '178.62.111.213', user: 'app', roles: %w{web app db util}
+server '178.62.109.10', user: 'app', roles: %w{web app db util}
